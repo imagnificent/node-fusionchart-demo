2db80d04a3306bdac41b6e9ebd8f0b0b23438357
imagnificent <30681086+imagnificent@users.noreply.github.com>
2019-05-05 08:49:22 +0800

    first commit

--- public/js/fusioncharts_demo.js
+++ public/js/fusioncharts_demo.js
@@ -1,39 +1,236 @@
 var chartData;
 
-$(function(){
-  $.ajax({
+$(function() {
+    $.ajax({
 
-    url: 'http://localhost:3300/fuelPrices',
-    type: 'GET',
-    success : function(data) {
-      chartData = data;
-      var template = Handlebars.compile($("#tabular-template").html());
-      $("#table-location").html(template(data));
+        url: 'http://localhost:3300/fuelPrices',
+        type: 'GET',
+        success: function(data) {
+            chartData = data;
+            //var template = Handlebars.compile($("#tabular-template").html());
+            //$("#table-location").html(template(data));
 
-      var chartProperties = {
-        "caption": "Variation of Petrol and Diesel price in Bangalore",
-        "numberprefix": "Rs",
-        "xAxisName": "Month",
-        "yAxisName": "Price"
-      };
 
-      var categoriesArray = [{
-          "category" : data["categories"]
-      }];
+            var chartProperties = {
+                "caption": "Reaction Time",
+                "numbersuffix": "s",
+                "xAxisName": "Month",
+                "yAxisName": "Time",
+                //bgcolor: "#FFFFFF",
+                canvasBgColor: "#FFFFFF",
+                showBorder: "1",
+                //showCanvasBorder: "0",
+                showLabels: "0",
+                drawCrossLine: "1",
+                divLineAlpha: "0",
+                //showYAxisValues: "0",
+                chartLeftMargin: "0",
+                chartRightMargin: "0",
+                canvasRightMargin: "25",
+                canvasLeftMargin: "0",
+                chartBottomMargin: "15",
+                canvasBottomMargin: "15",
+                chartTopMargin: "15",
+                canvasTopMargin: "15",
 
-      var lineChart = new FusionCharts({
-        type: 'msline',
-        renderAt: 'chart-location',
-        width: '1000',
-        height: '600',
-        dataFormat: 'json',
-        dataSource: {
-          chart: chartProperties,
-          categories : categoriesArray,
-          dataset : data["dataset"]
+                /*
+                showShadow: "0",
+                paletteColors: "#3273DC",
+                drawAnchors: "0",
+                showAlternateHGridColor: "0",
+                crossLineColor: "#363636",
+                crossLineAlpha: "15",
+                drawCrossLineOnTop: "0",
+                usePlotGradientColor: "1",
+                */
+                plotFillAlpha: "15",
+                plotColorinTooltip: "0",
+                tooltipBorderAlpha: "0",
+                toolTipPadding: "0",
+                baseFontColor: "#205BBB",
+                baseFontSize: "15",
+                baseFont: "Nunito",
+
+            };
+
+            var chartProperties2 = {
+                "caption": "Speed",
+                "numbersuffix": "cm/s",
+                "xAxisName": "Month",
+                "yAxisName": "Price"
+            };
+
+            var chartProperties3 = {
+                "caption": "Accuracy",
+                "numbersuffix": "%",
+                "xAxisName": "Month",
+                "yAxisName": "Price"
+            };
+
+            var chartProperties4 = {
+                "caption": "Reaching distance",
+                "numbersuffix": "cm",
+                "xAxisName": "Month",
+                "yAxisName": "Price"
+            };
+
+            var categoriesArray = [{
+                "category": data["categories"]
+            }];
+
+            var Chart = new FusionCharts({
+                type: 'msline',
+                renderAt: 'chart-location',
+                width: '500',
+                height: '300',
+                dataFormat: 'json',
+                dataSource: {
+                    //chart: chartCosmetics.currencyChart,
+                    chart: chartProperties,
+                    categories: categoriesArray,
+                    dataset: data["dataset"]
+                },
+                line: {
+                    "color": "#6baa01",
+                    "valueOnRight": "1",
+                }
+            });
+            Chart.render();
+
+            var Chart2 = new FusionCharts({
+                type: 'msline',
+                renderAt: 'chart-location2',
+                width: '500',
+                height: '300',
+                dataFormat: 'json',
+                dataSource: {
+                    chart: chartProperties2,
+                    categories: categoriesArray,
+                    dataset: data["dataset2"]
+                }
+
+
+            });
+            Chart2.render();
+
+            var Chart3 = new FusionCharts({
+                type: 'msline',
+                renderAt: 'chart-location3',
+                width: '500',
+                height: '300',
+                dataFormat: 'json',
+                dataSource: {
+                    chart: chartProperties3,
+                    categories: categoriesArray,
+                    dataset: data["dataset2"]
+                }
+
+
+            });
+            Chart3.render();
+
+            var Chart4 = new FusionCharts({
+                type: 'msline',
+                renderAt: 'chart-location4',
+                width: '500',
+                height: '300',
+                dataFormat: 'json',
+                dataSource: {
+                    chart: chartProperties4,
+                    categories: categoriesArray,
+                    dataset: data["dataset2"]
+                }
+
+
+            });
+            Chart4.render();
         }
-      });
-      lineChart.render();
-    }
-  });
+    });
 });
+
+chartCosmetics = {
+    currencyChart: {
+        caption: "Reaction Time",
+        numbersuffix: "s",
+        xAxisName: "Month",
+        yAxisName: "Price",
+        bgcolor: "#FFFFFF",
+        canvasBgColor: "#FFFFFF",
+        showBorder: "0",
+        showCanvasBorder: "0",
+        showLabels: "0",
+        drawCrossLine: "1",
+        divLineAlpha: "0",
+        showYAxisValues: "0",
+        chartLeftMargin: "0",
+        chartRightMargin: "0",
+        canvasRightMargin: "0",
+        canvasLeftMargin: "0",
+        chartBottomMargin: "0",
+        canvasBottomMargin: "0",
+        chartTopMargin: "0",
+        canvasTopMargin: "0",
+        showValues: "0",
+        shadow: "0",
+        legendPadding: "0",
+        showShadow: "0",
+        paletteColors: "#3273DC",
+        drawAnchors: "0",
+        showAlternateHGridColor: "0",
+        crossLineColor: "#363636",
+        crossLineAlpha: "15",
+        drawCrossLineOnTop: "0",
+        usePlotGradientColor: "1",
+        plotFillAlpha: "15",
+        plotColorinTooltip: "0",
+        tooltipBorderAlpha: "0",
+        toolTipPadding: "0",
+        baseFontColor: "#205BBB",
+        baseFontSize: "15",
+        baseFont: "Nunito",
+        tooltipbgalpha: "0",
+        plotFillAngle: "90",
+        numberPrefix: "$",
+        plotToolText: "<b>$label: $dataValue</b>"
+    },
+    percentChart: {
+        bgcolor: "#FFFFFF",
+        canvasBgColor: "#FFFFFF",
+        showBorder: "0",
+        showCanvasBorder: "0",
+        showLabels: "0",
+        drawCrossLine: "1",
+        divLineAlpha: "0",
+        showYAxisValues: "0",
+        chartLeftMargin: "0",
+        chartRightMargin: "0",
+        canvasRightMargin: "0",
+        canvasLeftMargin: "0",
+        chartBottomMargin: "0",
+        canvasBottomMargin: "0",
+        chartTopMargin: "0",
+        canvasTopMargin: "0",
+        showValues: "0",
+        shadow: "0",
+        legendPadding: "0",
+        showShadow: "0",
+        paletteColors: "#3273DC",
+        drawAnchors: "0",
+        showAlternateHGridColor: "0",
+        crossLineColor: "#363636",
+        crossLineAlpha: "15",
+        drawCrossLineOnTop: "0",
+        usePlotGradientColor: "1",
+        plotFillAlpha: "15",
+        plotColorinTooltip: "0",
+        tooltipBorderAlpha: "0",
+        toolTipPadding: "0",
+        baseFontColor: "#205BBB",
+        baseFontSize: "15",
+        baseFont: "Nunito",
+        tooltipbgalpha: "0",
+        plotFillAngle: "90",
+        numberSuffix: "%",
+        plotToolText: "<b>$label: $dataValue</b>"
+    }
+}
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require("express");
 //import mongodb package
 var mongodb = require("mongodb");
 
+
+const path = require('path');
 //MongoDB connection URL - mongodb://host:port/dbName
 var dbHost = "mongodb://localhost:27017/fusion_demo";
 
@@ -15,71 +17,97 @@ var MongoClient = mongodb.MongoClient;
 
 //Connecting to the Mongodb instance.
 //Make sure your mongodb daemon mongod is running on port 27017 on localhost
-MongoClient.connect(dbHost, function(err, db){
-  if ( err ) throw err;
-  dbObject = db;
+MongoClient.connect(dbHost, function(err, db) {
+    if (err) throw err;
+    dbObject = db;
 });
 
-function getData(responseObj){
-  //use the find() API and pass an empty query object to retrieve all records
-  dbObject.collection("fuel_price").find({}).toArray(function(err, docs){
-    if ( err ) throw err;
-    var monthArray = [];
-    var petrolPrices = [];
-    var dieselPrices = [];
+function getData(responseObj) {
+    //use the find() API and pass an empty query object to retrieve all records
+    dbObject.collection("patient").find({}).toArray(function(err, docs) {
+        if (err) throw err;
+        var monthArray = [];
+        var reactionTimeArray = [];
+        var speedArray = [];
+        var accuracyArray = [];
+        var reachingDistanceArray = [];
 
-    for ( index in docs){
-      var doc = docs[index];
-      //category array
-      var month = doc['month'];
-      //series 1 values array
-      var petrol = doc['petrol'];
-      //series 2 values array
-      var diesel = doc['diesel'];
-      monthArray.push({"label": month});
-      petrolPrices.push({"value" : petrol});
-      dieselPrices.push({"value" : diesel});
-    }
+        for (index in docs) {
+            var doc = docs[index];
+            //category array
+            var month = doc['month'];
+            //series 1 values array
+            var reactionTime = doc['reactionTime'];
+            //series 2 values array
+            var speed = doc['speed'];
+            //series 3 values array
+            var accuracy = doc['accuracy'];
+            //series 4 values array
+            var reachingDistance = doc['distance'];
+            monthArray.push({ "label": month });
+            reactionTimeArray.push({ "value": reactionTime });
+            speedArray.push({ "value": speed });
+            accuracyArray.push({ "value": accuracy });
+            reachingDistanceArray.push({ "value": reachingDistance });
+        }
 
-    var dataset = [
-      {
-        "seriesname" : "Petrol Price",
-        "data" : petrolPrices
-      },
-      {
-        "seriesname" : "Diesel Price",
-        "data": dieselPrices
-      }
-    ];
+        var dataset = [{
+            "seriesname": "Reaction Time",
+            "data": reactionTimeArray
+        }];
+        var dataset2 = [{
+            "seriesname": "Speed",
+            "data": speedArray
+        }];
+        var dataset3 = [{
+            "seriesname": "Accuracy",
+            "data": accuracyArray
+        }];
+        var dataset4 = [{
+            "seriesname": "Reaching Distance",
+            "data": reachingDistanceArray
+        }];
 
-    var response = {
-      "dataset" : dataset,
-      "categories" : monthArray
-    };
-    responseObj.json(response);
-  });
+
+        var response = {
+            "dataset": dataset,
+            "dataset2": dataset2,
+            "dataset3": dataset3,
+            "dataset4": dataset4,
+            "categories": monthArray
+        };
+        responseObj.json(response);
+    });
 }
 
 //create express app
 var app = express();
 
 //NPM Module to integrate Handlerbars UI template engine with Express
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 //Declaring Express to use Handlerbars template engine with main.handlebars as
 //the default layout
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+//app.set('view engine', 'handlebars');
+
+app.set('view engine', 'html');
 
 //Defining middleware to serve static files
-app.use('/public', express.static('public'));
-app.get("/fuelPrices", function(req, res){
-  getData(res);
+//app.use('/public', express.static('public'));
+app.get("/fuelPrices", function(req, res) {
+    getData(res);
 });
-app.get("/", function(req, res){
-  res.render("chart");
+app.use(express.static(path.join(__dirname, 'public')));
+// index routes
+app.get('/', function(req, res) {
+    getData(res);
+    res.render('/public/index.html');
 });
+//app.get("/", function(req, res) {
+//    res.render("chart");
+//});
 
-app.listen("3300", function(){
-  console.log('Server up: http://localhost:3300');
+app.listen("3300", function() {
+    console.log('Server up: http://localhost:3300');
 });
